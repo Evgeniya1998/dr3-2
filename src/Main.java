44fd7047c825bb19191d1978a8ef220d3622b748
Evgesha <Slav.novikova2014@gmail.com>
2023-05-10 10:42:50 +0700

    added Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,6 @@ public class Main {
         BmiService service = new BmiService();
         double weightKg = 98;
         double hightMetrs = 1.87;
-
-
         double index = service.calculate(weightKg, hightMetrs); // должно получиться 28
         System.out.println((int) index);
     }
